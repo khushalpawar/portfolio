1af82ba5e5e96c5b4dd46486bb8817f314a9fbd9
gauravlagad96 <gauravlagad2252001@gmail.com>
2024-06-08 16:19:08 +0530

    v1

---
+++ src/data/constants.js
@@ -0,0 +1,227 @@
+import portfolioScreen from "../images/portfolioimage.png"
+export const Bio = {
+  name: "Gaurav Lagad",
+  roles: [
+    "Web Developer",
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/gauravlagad96",
+  resume:
+    "https://drive.google.com/file/d/1eG3nrUgy5H4s2OhmTtDuZofdaCy3u-Hn/view?usp=drivesdk",
+  linkedin: "https://www.linkedin.com/in/gaurav-lagad-9431bb22b",
+  twitter: "#",
+  insta: "#",
+  facebook: "#",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://img.icons8.com/?size=96&id=x7XMNGh2vdqA&format=png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Spring",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Spring Boot",
+        image:
+          "https://img.icons8.com/?size=96&id=90519&format=png",
+      },
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      // {
+      //   name: "MongoDB",
+      //   image:
+      //     "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      // },
+      
+    ],
+  },
+  // {
+  //   title: "Android",
+  //   skills: [
+  //     {
+  //       name: "Java",
+  //       image:
+  //         "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+  //     },
+  //     {
+  //       name: "XML",
+  //       image:
+  //         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+  //     },
+  //     {
+  //       name: "Android Studio",
+  //       image:
+  //         "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+  //     },
+  //   ],
+  // },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "AWS",
+        image:
+          "https://img.icons8.com/?size=96&id=33039&format=png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://res.cloudinary.com/postman/image/upload/t_team_logo/v1629869194/team/2893aede23f01bfcbd2319326bc96a6ed0524eba759745ed6d73405a3a8b67a8",
+      },
+      {
+        name: "Git",
+        image:
+          "https://img.icons8.com/?size=96&id=20906&format=png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+
+      {
+        name: "Eclipse",
+        image:
+          "https://img.icons8.com/?size=160&id=rPAHs7H1vriV&format=png",
+      },
+      {
+        name: "STS",
+        image:
+          "https://spring.io/img/projects/spring-tool.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.imscdr.ac.in/public/images/frontend/logo.png",
+    school: "Institute of Management Studies Career Development & Research (IMSCDR), Ahilydevianagar",
+    date: "  Nov 2022 - May 2024",
+    grade: "8.32 CGPA",
+    desc: "I am currently pursuing a Master's degree in Computer application at Institute of Management Studies Career Development & Research (IMSCDR), Ahmednagar. I have completed 3 semesters and have a CGPA of 7.72. In the academic curriculum i have learn various subjects such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. where I am learning, improving skills and working on exciting projects.",
+    degree: "Master of Computer Application",
+  },
+  {
+    id: 1,
+    img: "https://files.prisms.in/scsmprisms/2021-2022/Academic/header_settings/SCSM.png",
+    school: "Shri Chhatrapati Shivaji Mahavidyalaya, Shrigonda",
+    date: "Apr 2019 - Apr 2022",
+    grade: "7.96 CGPA",
+    desc: "I completed my Bachelor's degree in Computer Application at Shri Chhatrapati Shivaji Mahavidyalaya, Shrigonda. Where i learn programming languages and computer fundamental concepts and also various skills such as C, java, frontend technologies and so on",
+    degree: "Bachelor Of Computer Application",
+  },
+  {
+    id: 2,
+    img: "https://img.icons8.com/?size=100&id=8TNnHFi2aBoF&format=png",
+    school: "Shri Kolaidevi Junior College, Kolgaon, Ahmednagar",
+    date: "Apr 2017 - Apr 2019",
+    grade: "64.0%",
+    desc: "I completed my class 12 high school education at Shri Kolaidevi Junior College Kolgaon, Kolgaon. where I studied Science with PCMB Group.",
+    degree: "HSC(XII), Science with PCMB Group",
+  },
+  {
+    id: 3,
+    img: "https://img.icons8.com/?size=100&id=8TNnHFi2aBoF&format=png",
+    school: "Shree Kolaidevi Vidyalaya Kolgaon,shrigonda.",
+    date: "Apr 2016 - Apr 2017",
+    grade: "74.80%",
+    desc: "I completed my class 10 education at Shree Kolaidevi Vidyalaya Kolgaon. where I completed my secondary education",
+    degree: "SSC(X), Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "food-ordering-system",
+    date: "Nov 2022 - Mar 2023",
+    description:
+      " The food ordering system is MERN Stack Ecommerce Project. The proposed system is developed to manage ordering activities in fast food restaurant. These systems will ultimately allow you to better manage resources ",
+    image:
+      {portfolioScreen},
+    tags: ["HTML5 & CSS3", "Bootstrap", "Javascript", "MERN Technology"],
+    category: "web app",
+    github: "",
+  },
+  {
+    id: 1,
+    title: "Agri-Boost",
+    date: "May 2023 - Jul 2023",
+    description:
+      "The Agri-Boost has been developed to manage the fertilizer in the store. This system improves the method of taking the order from customer. This system also provides a report generation system in which user can generate weekly, monthly, yearly reports.",
+    image:
+      "https://github.com/HusenMujawar/Agri-Boost/blob/main/Screenshot%202024-01-08%20141022.png?raw=true",
+    tags: ["Html", "CSS", "Javascript", "PHP", "MYSQL"],
+    category: "web app",
+    github: "https://github.com/HusenMujawar/Agri-Boost.git",
+  },
+  {
+    id: 2,
+    title: "Portfolio website",
+    date: "Jan 2024",
+    description:
+      "A portfolio react based responsive website. This project showcases my skills, projects, and experiences as a web developer.",
+    image:
+      "#",
+    tags: ["HTML5", "CSS", "Javascript", "ReactJS"],
+    category: "web app",
+    github: "https://github.com/HusenMujawar/Portfolio.git",
+  },
+
+];
+
